28f1df7ea46289ef546cf0ffc860f3e8971f0983
piyawat saikun <tonkaw.zaa@gmail.com>
2016-03-09 12:06:13 +0000

    Changes from Views service

--- listGifts/app.js
+++ listGifts/app.js
@@ -8,7 +8,6 @@
     var bootstrap = function() {
         $(function() {
             app.mobileApp = new kendo.mobile.Application(document.body, {
-                transition: 'slide',
                 skin: 'flat',
                 initial: 'components/home/view.html'
             });
@@ -19,6 +18,19 @@
         document.addEventListener('deviceready', function() {
             if (navigator && navigator.splashscreen) {
                 navigator.splashscreen.hide();
+            }
+
+            var element = document.getElementById('appDrawer');
+            if (typeof(element) != 'undefined' && element !== null) {
+                if (window.navigator.msPointerEnabled) {
+                    $('#navigation-container').on('MSPointerDown', 'a', function(event) {
+                        app.keepActiveState($(this));
+                    });
+                } else {
+                    $('#navigation-container').on('touchstart', 'a', function(event) {
+                        app.keepActiveState($(this));
+                    });
+                }
             }
 
             bootstrap();
--- listGifts/components/home/index.js
+++ listGifts/components/home/index.js
@@ -1,6 +1,23 @@
 'use strict';
 
 app.home = kendo.observable({
+    
+    top:"top",
+    /*
+    data: new kendo.data.DataSource({
+            transport: {
+                read: {
+                    //url: "components/data/books.js",
+                    url: "https://greenapi.odooportal.com/api/v1/event_gifts",
+                    type: "get",
+                    dataType: "json"
+                }
+            },
+        schema: {
+            data: "data"
+        }
+    }),*/
+                            
     onShow: function() {},
     afterShow: function() {}
 });
